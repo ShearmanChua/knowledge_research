dff381e9fdb2b156d697f75b3043d2942b00422f
ShearmanChua <shearman_chua@hotmail.com>
2025-12-05 16:09:05 +0800

    changed

--- agents/src/template_environment/configs/runtime_config.py
+++ agents/src/template_environment/configs/runtime_config.py
@@ -1,54 +1,11 @@
-# prompt = """Tell me more about the use of drones in the Ukraine War and generate a research report on them.
-# The report should include:
 
-# - An introduction of what kind of drones are used and how they work
-# - A discussion of the strengths and weaknesses of various drones
-# - How do drones compare to other forms of military equipment
-# - What are the unexplored areas of research in drone technology"""
-
-# prompt = """Tell me more about Diffusion Language Models and generate a research report on them.
-# The report should include:
-
-# - An introduction of what diffusion language models are and how they work
-# - A discussion of the strengths and weaknesses of diffusion language models
-# - How do diffusion language models compare to autoregressive language models
-# - What are the unexplored areas of research in diffusion language models
-# - How can diffusion language models be used in real-world applications
-# - How can diffussion language models solve problems that autoregressive language models cannot"""
-
-prompt = """Tell me more about how terrorism has evolved throughout the years and generate a research report on them.
+prompt = """Tell me more about Diffusion Language Models and generate a research report on them.
 The report should include:
 
-- Prominent terrorist groups and their tactics
-- How terrorism tactics has evolved over the years (starting from 2010s)
-- Types of terrorism tactics (What are the types of attacks conducted by terrorists)
-- Recruitments and training of terrorists
-- What are some of the trending terrorism tactics (e.g. IED based bombing etc)
-- When and where do terrorism attacks occur (temporal patterns)
-- In depth TTP analysis of prominent terrorist groups
-"""
+- An introduction of what diffusion language models are and how they work
+- A discussion of the strengths and weaknesses of diffusion language models
+- How do diffusion language models compare to autoregressive language models
+- What are the unexplored areas of research in diffusion language models
+- How can diffusion language models be used in real-world applications
+- How can diffussion language models solve problems that autoregressive language models cannot"""
 
-# prompt = """Generate a comprehensive report about the orbat of a Malaysian Army Division.
-# The report should include:
-
-# - Structure of a typical Malaysian Army Division(Make use of Mermaid diagram to show the orbat)
-# - The purpose of the different units of a Malaysian Army Division
-# - Notable exercises conducted by a Malaysian Army Division
-# - Equipment used by the different units of a Malaysian Army Division
-# - How a Malaysian Army Division is deployed for combat operations
-# - How an infantry batallion is deployed along with support elements
-#     * details on deployment of forces
-#     * weapons, armoured vehicles, and other support elements deployed along with the batallion
-# """
-
-# prompt = """Generate a comprehensive report about the equipment used by the Malaysian Maritime Enforcement Agency
-# The report should include:
-
-# - Types of equipment used by the Malaysian Maritime Enforcement Agency
-# - Current equipment used by the Malaysian Maritime Enforcement Agency 
-#     * number of units for each equipment (leave as Unknown if no information found)
-#     * possible configurations (for large equipment such as ships etc)
-#     * any potential weakness to grey zone maritime tactics
-# - Future equipment/ equipment being procured/developed for the Malaysian Maritime Enforcement Agency
-#     * how do they enhance the capabilities of MMEA
-# """
